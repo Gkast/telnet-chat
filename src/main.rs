b6ffbdc763f099d759ad6a745c347178a0621af3
george <gkast.ath@gmail.com>
2023-08-24 23:15:05 +0300

    reverted

--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,46 @@
-use std::sync::Arc;
 use tokio::{
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
-    signal::ctrl_c,
-    signal::unix::{signal, SignalKind},
-    sync::{broadcast, Mutex},
+    sync::broadcast,
 };
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let pid = std::process::id();
-    println!("Server started with PID: {}", pid);
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let listener = TcpListener::bind("localhost:8080").await?;
     let (tx, _rx) = broadcast::channel(10);
-    let tx = Arc::new(Mutex::new(tx));
-
-    // Spawn a task to handle the SIGINT signal (Ctrl+C)
-    let (shutdown_tx, mut shutdown_rx) = tokio::sync::mpsc::channel(1);
-    let tx_clone = Arc::clone(&tx);
-    tokio::spawn(async move {
-        let _ = ctrl_c().await;
-        println!("Received Ctrl+C (PID: {}). Initiating graceful shutdown...", pid);
-
-        // Inform other tasks about shutdown
-        let _ = tx_clone.lock().await.send((
-            format!("Server (PID: {}) is shutting down due to Ctrl+C", pid),
-            std::net::SocketAddr::from(([0, 0, 0, 0], 0)),
-        ));
-
-        // Send a message to the main thread to initiate shutdown
-        let _ = shutdown_tx.send(()).await;
-    });
-
-    // Spawn a task to handle the SIGTERM signal
-    let (shutdown_tx_sigterm, mut shutdown_rx_sigterm) = tokio::sync::mpsc::channel(1);
-    let tx_clone_sigterm = Arc::clone(&tx);
-    tokio::spawn(async move {
-        let mut sigterm = signal(SignalKind::terminate()).unwrap();
-        sigterm.recv().await;
-        println!("Received SIGTERM (PID: {}). Initiating graceful shutdown...", pid);
-
-        // Inform other tasks about shutdown
-        let _ = tx_clone_sigterm.lock().await.send((
-            format!("Server (PID: {}) is shutting down due to SIGTERM", pid),
-            std::net::SocketAddr::from(([0, 0, 0, 0], 0)),
-        ));
-
-        // Send a message to the main thread to initiate shutdown
-        let _ = shutdown_tx_sigterm.send(()).await;
-    });
-
-    let mut connections = vec![];
 
     loop {
-        tokio::select! {
-            result = listener.accept() => {
-                let (socket, addr) = result?;
-                let tx_clone = Arc::clone(&tx);
+        let (mut socket, addr) = listener.accept().await.unwrap();
 
-                // Spawn a task to handle the connection
-                let connection = tokio::spawn(handle_connection(socket, addr, tx_clone));
-                connections.push(connection);
-            }
-            _ = shutdown_rx.recv() => {
-                // Received shutdown signal, break the loop and exit
-                println!("Shutting down gracefully due to Ctrl+C (PID: {})...", pid);
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
-                // Wait for all connection handling tasks to finish
-                for connection in connections {
-                    let _ = connection.await; // Wait for the task to complete
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0{
+                            break;
+                        }
+
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let (msg, other_addr) = result.unwrap();
+
+                        if addr != other_addr{
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+                    }
                 }
-
-                break;
             }
-            _ = shutdown_rx_sigterm.recv() => {
-                // Received shutdown signal, break the loop and exit
-                println!("Shutting down gracefully due to SIGTERM (PID: {})...", pid);
-
-                // Wait for all connection handling tasks to finish
-                for connection in connections {
-                    let _ = connection.await; // Wait for the task to complete
-                }
-
-                break;
-            }
-        }
+        });
     }
-
-    Ok(())
-}
-
-async fn handle_connection(
-    mut socket: tokio::net::TcpStream,
-    addr: std::net::SocketAddr,
-    tx: Arc<Mutex<tokio::sync::broadcast::Sender<(String, std::net::SocketAddr)>>>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let (reader, mut _writer) = socket.split();
-    let mut reader = BufReader::new(reader);
-    let mut line = String::new();
-
-    loop {
-        tokio::select! {
-            result = reader.read_line(&mut line) => {
-                let bytes_read = result?;
-                if bytes_read == 0 {
-                    break;
-                }
-
-                if let Err(e) = tx.lock().await.send((line.clone(), addr)) {
-                    eprintln!("Error broadcasting message: {:?}", e);
-                }
-                line.clear();
-            }
-            _ = tokio::time::sleep(std::time::Duration::from_millis(10)) => {
-                // Sleep briefly to avoid busy-waiting
-            }
-        }
-    }
-
-    println!("Connection from {} has been gracefully closed.", addr);
-    Ok(())
 }
